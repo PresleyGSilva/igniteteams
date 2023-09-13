b04c1af0436477ba19bea8a6e51ad64e2504d6a1
Presley <presleygs.1607@gmail.com>
2023-09-12 22:42:21 -0300

    Estrutura do projeto - Path Mapping

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@utils': './src/utils',
+          '@theme': './src/theme',
+        }
+      }
+    ]
   };
 };
