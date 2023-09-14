bdcccbe611fb88e570852967dce6f4bd56c240c7
Presley <presleygs.1607@gmail.com>
2023-09-14 10:43:23 -0300

    CSS-in-JS - Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator} from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import  theme  from '@theme/index';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups';
+
+
 export default function App() {
+  const [fontsloaded] =useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+   {fontsloaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
   
 }
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { View, StyleSheet,Text } from "react-native";
+import { Container,  } from "./styles";
+export function Groups() {
+  return(
+    <Container>
+      
+    </Container>
+  )
+}
+
